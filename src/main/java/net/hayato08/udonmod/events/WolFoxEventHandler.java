ddd07613a8dadb8b84727c0205b1dad2b1c7097d
Hayato08 <sa.hayato509@gmail.com>
2025-03-12 04:58:38 +0900

    Wolfoxがワールド終了時にデスポーンするように修正

--- src/main/java/net/hayato08/udonmod/events/WolFoxEventHandler.java
+++ src/main/java/net/hayato08/udonmod/events/WolFoxEventHandler.java
@@ -7,17 +7,19 @@ import net.minecraft.world.entity.Entity;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @EventBusSubscriber(modid = UdonMod.MOD_ID)
 public class WolFoxEventHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WolFoxEventHandler.class);
+
     @SubscribeEvent
-    public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) // プレイヤーがログインしたとき
-    {
+    public static void onPlayerLoggedOut(PlayerEvent.PlayerLoggedOutEvent event) {
         if (event.getEntity().level() instanceof ServerLevel serverLevel) {
-            // 既存のWolfoxをすべて削除
             for (Entity entity : serverLevel.getAllEntities()) {
-                if (entity instanceof WolFoxEntity) {
+                if (entity instanceof WolFoxEntity wolFox) {
                     entity.discard();
                 }
             }
