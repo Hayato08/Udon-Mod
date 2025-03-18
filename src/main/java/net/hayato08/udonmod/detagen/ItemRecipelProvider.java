678f8bf2dbc6b3251bb91f9ca18919006a4f55b4
Hayato08 <sa.hayato509@gmail.com>
2025-03-18 12:25:40 +0900

    アーマーと刀のレシピを追加

--- src/main/java/net/hayato08/udonmod/detagen/ItemRecipelProvider.java
+++ src/main/java/net/hayato08/udonmod/detagen/ItemRecipelProvider.java
@@ -7,8 +7,10 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.common.Tags;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class ItemRecipelProvider extends RecipeProvider {
@@ -19,6 +21,15 @@ public class ItemRecipelProvider extends RecipeProvider {
     @Override
     protected void buildRecipes(RecipeOutput recipeOutput)
     {
+        List<ItemLike> RAW_UDON_SMELTING = List.of(UdonItems.RAW_UDON);
+        List<ItemLike> BOILED_UDON_BLASTING = List.of(UdonItems.BOILED_UDON);
+
+        oreSmelting(recipeOutput, RAW_UDON_SMELTING, RecipeCategory.MISC, UdonItems.BOILED_UDON,0.25f, 200, "boiled_udon");
+        oreBlasting(recipeOutput, RAW_UDON_SMELTING, RecipeCategory.MISC, UdonItems.BOILED_UDON,0.25f, 100, "boiled_udon");
+
+        oreSmelting(recipeOutput, BOILED_UDON_BLASTING, RecipeCategory.MISC, UdonItems.DRY_UDON,0.25f, 200, "dry_udon");
+        oreBlasting(recipeOutput, BOILED_UDON_BLASTING, RecipeCategory.MISC, UdonItems.DRY_UDON,0.25f, 100, "dry_udon");
+
 
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonBlocks.STONE_MILL.get())
                 .pattern("ABB")
@@ -56,12 +67,7 @@ public class ItemRecipelProvider extends RecipeProvider {
                 .define('C', UdonItems.RICH_DASHI)
                 .unlockedBy("has_boiled_udon", has(UdonItems.BOILED_UDON)).save(recipeOutput);
 
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.UDON_ROPE.get())
-                .pattern("AB")
-                .define('A', UdonItems.RAW_UDON)
-                .define('B', Items.SLIME_BALL)
-                .unlockedBy("has_slime_ball", has(Items.SLIME_BALL)).save(recipeOutput);
-
+        // Kitsune
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.KITSUNE_HELMET.get())
                 .pattern("AAA")
                 .pattern("A A")
@@ -87,6 +93,160 @@ public class ItemRecipelProvider extends RecipeProvider {
                 .pattern("A A")
                 .define('A', UdonItems.OAGE)
                 .unlockedBy("has_oage", has(UdonItems.OAGE.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.KITSUNE_KATANA.get())
+                .pattern("A")
+                .pattern("A")
+                .pattern("B")
+                .define('B', Tags.Items.RODS_WOODEN)
+                .define('A', UdonItems.OAGE)
+                .unlockedBy("has_oage", has(UdonItems.OAGE)).save(recipeOutput);
+
+
+        // Bukkake
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.BUKKAKE_HELMET.get())
+                .pattern("AAA")
+                .pattern("A A")
+                .define('A', UdonItems.BUKKAKE_UDON)
+                .unlockedBy("has_bukkake_udon", has(UdonItems.BUKKAKE_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.BUKKAKE_CHESTPLATE.get())
+                .pattern("A A")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', UdonItems.BUKKAKE_UDON)
+                .unlockedBy("has_bukkake_udon", has(UdonItems.BUKKAKE_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.BUKKAKE_LEGGINGS.get())
+                .pattern("AAA")
+                .pattern("A A")
+                .pattern("A A")
+                .define('A', UdonItems.BUKKAKE_UDON)
+                .unlockedBy("has_bukkake_udon", has(UdonItems.BUKKAKE_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.BUKKAKE_BOOTS.get())
+                .pattern("A A")
+                .pattern("A A")
+                .define('A', UdonItems.BUKKAKE_UDON)
+                .unlockedBy("has_bukkake_udon", has(UdonItems.BUKKAKE_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.BUKKAKE_KATANA.get())
+                .pattern("A")
+                .pattern("A")
+                .pattern("B")
+                .define('B', Tags.Items.RODS_WOODEN)
+                .define('A', UdonItems.BUKKAKE_UDON)
+                .unlockedBy("has_bukkake_udon", has(UdonItems.BUKKAKE_UDON)).save(recipeOutput);
+
+
+        // curry
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.CURRY_HELMET.get())
+                .pattern("AAA")
+                .pattern("A A")
+                .define('A', UdonItems.CURRY_UDON)
+                .unlockedBy("has_curry_udon", has(UdonItems.CURRY_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.CURRY_CHESTPLATE.get())
+                .pattern("A A")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', UdonItems.CURRY_UDON)
+                .unlockedBy("has_curry_udon", has(UdonItems.CURRY_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.CURRY_LEGGINGS.get())
+                .pattern("AAA")
+                .pattern("A A")
+                .pattern("A A")
+                .define('A', UdonItems.CURRY_UDON)
+                .unlockedBy("has_curry_udon", has(UdonItems.CURRY_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.CURRY_BOOTS.get())
+                .pattern("A A")
+                .pattern("A A")
+                .define('A', UdonItems.CURRY_UDON)
+                .unlockedBy("has_curry_udon", has(UdonItems.CURRY_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.CURRY_KATANA.get())
+                .pattern("A")
+                .pattern("A")
+                .pattern("B")
+                .define('B', Tags.Items.RODS_WOODEN)
+                .define('A', UdonItems.CURRY_UDON)
+                .unlockedBy("has_curry_udon", has(UdonItems.CURRY_UDON)).save(recipeOutput);
+
+        // Cold
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.COLD_HELMET.get())
+                .pattern("AAA")
+                .pattern("A A")
+                .define('A', UdonItems.COLD_UDON)
+                .unlockedBy("has_cold_udon", has(UdonItems.COLD_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.COLD_CHESTPLATE.get())
+                .pattern("A A")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', UdonItems.COLD_UDON)
+                .unlockedBy("has_cold_udon", has(UdonItems.COLD_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.COLD_LEGGINGS.get())
+                .pattern("AAA")
+                .pattern("A A")
+                .pattern("A A")
+                .define('A', UdonItems.COLD_UDON)
+                .unlockedBy("has_cold_udon", has(UdonItems.COLD_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.COLD_BOOTS.get())
+                .pattern("A A")
+                .pattern("A A")
+                .define('A', UdonItems.COLD_UDON)
+                .unlockedBy("has_cold_udon", has(UdonItems.COLD_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.COLD_KATANA.get())
+                .pattern("A")
+                .pattern("A")
+                .pattern("B")
+                .define('B', Tags.Items.RODS_WOODEN)
+                .define('A', UdonItems.COLD_UDON)
+                .unlockedBy("has_cold_udon", has(UdonItems.COLD_UDON)).save(recipeOutput);
+
+
+        // Zaru
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.ZARU_HELMET.get())
+                .pattern("AAA")
+                .pattern("A A")
+                .define('A', UdonItems.ZARU_UDON)
+                .unlockedBy("has_zaru_udon", has(UdonItems.ZARU_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.ZARU_CHESTPLATE.get())
+                .pattern("A A")
+                .pattern("AAA")
+                .pattern("AAA")
+                .define('A', UdonItems.ZARU_UDON)
+                .unlockedBy("has_zaru_udon", has(UdonItems.ZARU_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.ZARU_LEGGINGS.get())
+                .pattern("AAA")
+                .pattern("A A")
+                .pattern("A A")
+                .define('A', UdonItems.ZARU_UDON)
+                .unlockedBy("has_zaru_udon", has(UdonItems.ZARU_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.ZARU_BOOTS.get())
+                .pattern("A A")
+                .pattern("A A")
+                .define('A', UdonItems.ZARU_UDON)
+                .unlockedBy("has_zaru_udon", has(UdonItems.ZARU_UDON.get())).save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, UdonItems.ZARU_KATANA.get())
+                .pattern("A")
+                .pattern("A")
+                .pattern("B")
+                .define('B', Tags.Items.RODS_WOODEN)
+                .define('A', UdonItems.ZARU_UDON)
+                .unlockedBy("has_zaru_udon", has(UdonItems.ZARU_UDON)).save(recipeOutput);
+
+
+
 
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, UdonItems.RAW_UDON.get())
                 .requires(UdonItems.FLOUR)
